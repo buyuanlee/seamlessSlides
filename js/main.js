ba92699ce64ed76109d4677693bc1f74271b5514
buyuanlee <buyuanlee@gmail.com>
2018-09-02 00:42:47 +0800

    第一版-自动轮播

--- js/main.js
+++ js/main.js
@@ -1,9 +1,48 @@
-$('.images>img').eq(0).addClass('current')
-$('.images>img').eq(1).addClass('enter')
-setTimeout(() => {
-    $('.images>img').eq(0).removeClass('current').addClass('leave')
-        .one('transitionend', (x) => {
-            $(x.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $('.images>img').eq(1).removeClass('enter').addClass('current')
-}, 3000)
+
+let n
+initialization()
+setInterval(()=>{
+    makeLeave(getImg(n))
+    .one('transitionend', (e)=>{
+    makeEnter($(e.currentTarget))
+})
+makeCurrent(getImg(n+1))
+n += 1
+},2000)
+
+
+
+//函数库
+
+//控制n的数值
+function x(n) {
+    if (n > 4) {
+        n = n % 5
+    }
+    return n    //n = 0,1,2,3,4
+}
+
+//初始化函数
+function initialization() {
+    n = 0
+    $('.images>img').eq(0).addClass('current')
+        .siblings().addClass('enter')
+}
+
+//获取播放图片
+function getImg(n) {
+    return $('.images>img').eq(x(n))
+}
+
+//播放状态
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current')
+}
+//离开状态
+function makeLeave($node) {
+    return $node.removeClass('enter current').addClass('leave')
+}
+//进入状态
+function makeEnter($node) {
+    return $node.removeClass('current leave').addClass('enter')
+}
